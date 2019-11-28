a9316560153f6c716d32681f6362c7cc0cddf282
shyClass <shyoffice00@gmail.com>
2019-11-28 23:09:25 +0900

    new flight_hotel_reserve

---
+++ seat_insert.sql
@@ -0,0 +1,48 @@
+DELETE seats;
+
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('A01', '일반석', '01', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('A02', '일반석', '02', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('A03', '일반석', '03', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('A04', '일반석', '04', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('A05', '일반석', '05', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('A06', '일반석', '06', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('A07', '일반석', '07', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('A08', '일반석', '08', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('A09', '일반석', '09', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('A10', '일반석', '10', '150000');
+
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('B01', '프리미엄 일반석', '01', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('B02', '프리미엄 일반석', '02', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('B03', '프리미엄 일반석', '03', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('B04', '프리미엄 일반석', '04', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('B05', '프리미엄 일반석', '05', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('B06', '프리미엄 일반석', '06', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('B07', '프리미엄 일반석', '07', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('B08', '프리미엄 일반석', '08', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('B09', '프리미엄 일반석', '09', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('B10', '프리미엄 일반석', '10', '150000');
+
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('C01', '비즈니스석', '01', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('C02', '비즈니스석', '02', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('C03', '비즈니스석', '03', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('C04', '비즈니스석', '04', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('C05', '비즈니스석', '05', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('C06', '비즈니스석', '06', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('C07', '비즈니스석', '07', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('C08', '비즈니스석', '08', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('C09', '비즈니스석', '09', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('C10', '비즈니스석', '10', '150000');
+
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('D01', '일등석', '01', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('D02', '일등석', '02', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('D03', '일등석', '03', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('D04', '일등석', '04', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('D05', '일등석', '05', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('D06', '일등석', '06', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('D07', '일등석', '07', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('D08', '일등석', '08', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('D09', '일등석', '09', '150000');
+INSERT INTO seats (seat_code, seat_type, seat_num, f_price) VALUES('D10', '일등석', '10', '150000');
+
+
+COMMIT;

441fa74f432281de6a559c01b038c5ea92c174f5
shyClass <shyoffice00@gmail.com>
2019-11-30 01:39:39 +0900

    set up environment in labtop

--- TravelArchitect/src/main/java/com/ta/view/page/PageController.java
+++ TravelArchitect/src/main/java/com/ta/view/page/PageController.java
@@ -1,8 +1,5 @@
 package com.ta.view.page;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
